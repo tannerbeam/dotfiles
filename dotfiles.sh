1da9519c34dbc1ec1472d3c0c6820fb518735e65
Tanner Beam <tanner.beam@autodesk.com>
2019-12-19 14:11:14 -0800

    move dotfiles to ~/git/dotfiles | update gitignore

--- dotfiles.sh
+++ dotfiles.sh
@@ -15,11 +15,11 @@ echo "OK, cool."
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
-ln -sf ~/.dotfiles/zshrc ~/.zshrc
-ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/gitignore ~/.gitignore
-ln -sf ~/.dotfiles/Conda.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-ln -sf ~/.dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-ln -sf ~/.dotfiles/condarc ~/.condarc
+ln -nsf ~/git/dotfiles/zshrc ~/.zshrc
+ln -nsf ~/git/dotfiles/gitconfig ~/.gitconfig
+ln -nsf ~/git/dotfiles/gitignore ~/.gitignore
+ln -nsf ~/git/dotfiles/Conda.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+ln -nsf ~/git/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+ln -nsf ~/git/dotfiles/condarc ~/.condarc
 
 echo "All done!"
